8fe894a791cb5d99952ec8c8ecaa1c964b925ab4
ragnor <ragnor@aravinds-MacBook-Pro.local>
2026-04-29 14:21:11 -0500

    account reconcilation-import fix

--- lib/features/accounts/accounts_screen.dart
+++ lib/features/accounts/accounts_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../shared/models/account.dart';
 import '../../shared/models/transaction.dart';
 import 'accounts_provider.dart';
